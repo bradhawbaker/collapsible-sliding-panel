1fb18f8d67d564e416731b528c2d553cad248688
stormuicomponents <stormuicomponents@gmail.com>
2019-06-24 16:38:35 -0400

    cleaning up webpack

--- webpack.core.config.js
+++ webpack.core.config.js
@@ -1,4 +1,6 @@
 // Core/base webpack config (to be combined with prod or dev config
+const CircularDependencyPlugin = require('circular-dependency-plugin')
+
 module.exports = {
   resolve: {
     extensions: ['*', '.js', '.jsx']
@@ -38,5 +40,15 @@ module.exports = {
         exclude: /node_modules/,
       }
     ]
-  }
+  },
+  plugins: [
+    new CircularDependencyPlugin({
+      // exclude detection of files based on a RegExp
+      exclude: /a\.js|node_modules/,
+      // add errors to webpack instead of warnings
+      failOnError: true,
+      // set the current working directory for displaying module paths
+      cwd: process.cwd(),
+    })
+  ]
 };
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 const merge = require('webpack-merge');
 const core = require('./webpack.core.config');
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const TerserJSPlugin = require('terser-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = merge(core, {
+  mode: 'production',
   devtool: 'source-map',
   entry: {
-    'collapsible-sliding-panel' : [
-      path.join(__dirname, './index.js')
-    ]
+    'collapsible-sliding-panel' : './index.js'
   },
   output: {
     path:  path.join(__dirname, 'dist'),
-    publicPath: '/',
     filename: '[name].bundle.js',
+    publicPath: '/',
     library: 'collapsibleSlidingPanel',
     libraryTarget: 'umd'
   },
@@ -23,22 +23,25 @@ module.exports = merge(core, {
     'react',
     'prop-types'
   ],
-  mode: 'production',
   module: {
     rules: [
       {
         test: /\.(css|sass|scss)$/,
         use: [
-
-          'style-loader', 'css-loader','sass-loader'
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader" //1. Turns sass into css
         ]
       }
     ]
   },
+  plugins: [
+    new CleanWebpackPlugin()
+  ],
   optimization: {
-    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
+    minimizer: [
+      new TerserJSPlugin({ sourceMap: true }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
   }
 });
